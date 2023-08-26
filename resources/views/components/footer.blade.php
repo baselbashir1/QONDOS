a287173ba014db9696ff0a34a29f3da2134aab00
Basel Bashir <eng.baselbashir@gmail.com>
2023-08-26 23:53:08 +0300

    fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="footer-wrapper">
+<div class="footer-wrapper" style="position: fixed; bottom: 0">
     <div class="footer-section f-section-1">
         <p class="">Copyright © <span class="dynamic-year">2023</span> <a href="#" style="color: blue">Golden
                 Soft</a>, All rights reserved.</p>
